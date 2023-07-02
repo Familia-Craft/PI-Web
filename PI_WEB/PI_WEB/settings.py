9c580a69c6aafb2f92fd975ff58b5d8a6989424d
Ytr00m <dan.ro.sousa@gmail.com>
2023-07-02 12:51:05 -0300

    Version 0.2 - login e cadastro adicionados

--- PI_WEB/PI_WEB/settings.py
+++ PI_WEB/PI_WEB/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sistemaApice',
-        'USER': 'admin',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
