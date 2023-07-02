8d5a677a3e64431ade77444a833270e436c59739
Ytr00m <dan.ro.sousa@gmail.com>
2023-07-02 12:58:38 -0300

    Resolvendo conflitos

--- PI_WEB/PI_WEB/views/home.py
+++ PI_WEB/PI_WEB/views/home.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from PI_WEB.forms import LoginForm, ServidorForm
 from django.http import HttpRequest
-from django.contrib import auth
 
 @login_required
 def home(request: HttpRequest):
-    print(request)
     return render(request, "home.html")
 
